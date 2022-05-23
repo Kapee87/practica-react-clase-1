38c9a1ab49b77525a1eb7b63df1a4a99341b713d
Kapee87 <kapserker@gmail.com>
2022-05-23 19:14:19 -0300

    ultimos cambios de clase-2

--- src/App.js
+++ src/App.js
@@ -1,47 +1,43 @@
 import './App.css';
-import Lista from './components/Lista.js'
+
 
 function App() {
-const clase = {
-  numero : '5',
-  tema : 'Angular'
-}
 
+  const estilos = {
+    div1: { backgroundColor: 'red', width: 650, height: 400, margin: '0 auto' },
+    div2: { backgroundColor: 'yellow', width: 450, height: 300, margin: '0 auto' },
+    div3: { backgroundColor: 'green', width: 300, height: 200, margin: '0 auto', 
+      display: 'flex', justifyContent: 'center', alignItems: 'center' },
+    btn: { height: 50 },
+  }
 
+  function eventHandler(e) {
+    if (e.target.style.backgroundColor == 'red'){
+      e.target.style.backgroundColor = 'blue';
+    }else{
+      e.target.style.backgroundColor = 'red';
 
- const estilos = {
-   color:'blue',
-   fontSize: 30,
- }
+    }
+  }
 
-const autos = [
-  {id: 1, strong: 'Corsa', span:'Chevrolet'},
-  {id: 2, strong: 'Siena', span:'Fiat'},
-  {id: 3, strong: 'Ka', span:'Ford'},
-  {id: 4, strong: 'Gol', span:'Volkswagen'},
-  {id: 5, strong: 'A7', span:'Audi'},
-  {id: 6, strong: 'Sandero', span: 'Renault'}
-]
-
-const electrodomésticos = [
-  {id: 1, strong: 'Heladera', span:'Gafa'},
-  {id: 2, strong: 'Secarropas', span:'Kohinoor'},
-  {id: 3, strong: 'Cocina', span:'Escorial'},
-  {id: 4, strong: 'Batidora', span:'Peabody'},
-  {id: 5, strong: 'Aire acondicionado', span:'Lift'}
-]
-
-const esLigth= true;
+  function detenerPropagacion(e) {
+    e.stopPropagation();
+  }
 
   return (
-    <div className={App}>
-        <h2 style={estilos} > Bienvenidos a la clase {clase.numero} de {clase.tema}  </h2>
-          <Lista lista={autos} titulo='Lista de Autos' />
-          <Lista lista={electrodomésticos} titulo='Lista de Electrodomésticos'>
-            <li style={{backgroundColor: 'red'}}>Fin de la lista</li>
-            </Lista>
+    <div style={estilos.div1} onClick={eventHandler}>
+
+      <div style={estilos.div2} onClick={detenerPropagacion}> 
+
+        <div style={estilos.div3}>
+          
+          <button style={estilos.btn}>Presioname</button>
+        
+        </div>
+      
+      </div>
+
     </div>
   );
 }
-
 export default App;
